dac901fec339642345d709a6466839b450256637
mahmad0 <ahmadworkk007@gmail.com>
2024-06-05 13:26:05 +0500

    my project

---
+++ jjquery.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $("#faq1").click(function () {
+        $("#faqb1").slideToggle("slow");   
+    });
+    $("#faq2").click(function () {
+        $("#faqb2").slideToggle("slow");   
+    });
+    $("#faq3").click(function () {
+        $("#faqb3").slideToggle("slow");   
+    });
+    $("#faq4").click(function () {
+        $("#faqb4").slideToggle("slow");   
+    });
+    $("#faq5").click(function () {
+        $("#faqb5").slideToggle("slow");   
+    });
+});
